f9c71bd8341bc33dc288ecf795555e1dbc70ad56
Nicolas Caceda <nicolas.caceda@gmail.com>
2018-11-16 10:44:52 -0500

    Aesthetics and bug fixes

--- src/hscode/Controller.java
+++ src/hscode/Controller.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
